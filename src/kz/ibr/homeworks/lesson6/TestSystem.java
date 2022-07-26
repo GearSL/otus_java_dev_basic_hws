5cd51b0efa2f90d73588a7784d4774a185c0e05e
ilyas <i.ismailov@dengiclick.kz>
2022-07-26 20:36:18 +0600

    HW6: выполнен первый пункт доработок к ДЗ

--- src/kz/ibr/homeworks/lesson6/TestSystem.java
+++ src/kz/ibr/homeworks/lesson6/TestSystem.java
@@ -1,5 +1,6 @@
 package kz.ibr.homeworks.lesson6;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -10,34 +11,34 @@ public class TestSystem {
 
         int correctCount = 0, wrongCount = 0;
 
-        String[] questions = {
-                "Вопрос 1: правильный ответ 1",
-                "Вопрос 2: правильный ответ 2",
-                "Вопрос 3: правильный ответ 3",
+        String[][][] testExamples = {
+                {
+                    {"Вопрос 1: правильный ответ 1", "Вопрос 2: правильный ответ 2", "Вопрос 3: правильный ответ 3",}
+                },
+                {
+                    {"Вариант ответа 1 для вопроса 1","Вариант ответа 2 для вопроса 1","Вариант ответа 3 для вопроса 1"},
+                    {"Вариант ответа 1 для вопроса 2","Вариант ответа 2 для вопроса 2","Вариант ответа 3 для вопроса 2"},
+                    {"Вариант ответа 1 для вопроса 3","Вариант ответа 2 для вопроса 3","Вариант ответа 3 для вопроса 3"},
+                },
+                {
+                    {"1", "2", "3"},
+                },
         };
-
-        String[][] answerOptions = {
-                {"Вариант ответа 1 для вопроса 1","Вариант ответа 2 для вопроса 1","Вариант ответа 3 для вопроса 1"},
-                {"Вариант ответа 1 для вопроса 2","Вариант ответа 2 для вопроса 2","Вариант ответа 3 для вопроса 2"},
-                {"Вариант ответа 1 для вопроса 3","Вариант ответа 2 для вопроса 3","Вариант ответа 3 для вопроса 3"},
-        };
-
-        int[] correctAnswers = {1, 2, 3};
 
         Scanner scanner = new Scanner(System.in);
 
-        for(int i = 0; i < questions.length; i++) {
-            // Вывод вопроса на экран
-            System.out.println(questions[i]);
+        for(int i = 0; i < testExamples[0][0].length; i++) {
+            // Вывод вопроса на экран.
+            System.out.println(testExamples[0][0][i]);
 
-            // Вывод вариантов ответов на экран
-            for(int j = 0; j < answerOptions[i].length; j++){
-                System.out.println(answerOptions[i][j] + ";");
+            // Вывод вариантов ответов на экран.
+            for(int j = 0; j < testExamples[1][1].length; j++){
+                System.out.println(testExamples[1][i][j] + ";");
             }
             System.out.print("Ваш ответ: ");
-            // Считываем с консоли ответ пользователя
-            // Проверяем ответ увеличиваем счетчики и выводим результат
-            if (correctAnswers[i] == scanner.nextInt()) {
+            // Считываем с консоли ответ пользователя.
+            // Проверяем ответ увеличиваем счетчики и выводим результат.
+            if (Objects.equals(testExamples[2][0][i], scanner.nextLine())) {
                 correctCount++;
             } else {
                 wrongCount++;
