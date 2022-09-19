7788219df2d99b9f4a9dfae975bc0adda2c520cd
ilyas <i.ismailov@dengiclick.kz>
2022-09-19 23:32:48 +0600

    HW19: Small developments for addRow

--- src/main/java/kz/ibr/homeworks/lesson19/asserts/js.js
+++ src/main/java/kz/ibr/homeworks/lesson19/asserts/js.js
@@ -1,12 +1,23 @@
 function addRow() {
     let table = document.querySelector("#studentsTable tbody");
+    let newRow = table.insertRow(table.rows.length - 2);
+    let studentNameCell = newRow.insertCell(0);
+    let studentGenderCell = newRow.insertCell(1);
+    let studentDateOBirthCell = newRow.insertCell(2);
+    let studentAgeCell = newRow.insertCell(3);
+
+    let studentName = document.createTextNode('Some name');
+    let studentGender = document.createTextNode('Male');
+    let studentDateOBirth = document.createTextNode('10/12/2000');
+    let studentAge = document.createTextNode(table.rows.length);
+
+    studentNameCell.appendChild(studentName);
+    studentGenderCell.appendChild(studentGender);
+    studentDateOBirthCell.appendChild(studentDateOBirth);
+    studentAgeCell.appendChild(studentAge);
+
     console.log(table);
     document.createElement("tr");
-    //table.append("<tr><td>AddRow</td><td>female</td><td>03/03/2015</td><td>15</td></tr>");
-    // table.append(document.createElement("tr"));
-    // table.append(document.createElement("td"));
-    //= "<tr><td>AddRow</td><td>female</td><td>03/03/2015</td><td>15</td></tr>" + table.innerHTML;
-    //table.innerHTML += "<tr><td>BOTTOM</td><td>BB</td></tr>";
 
     updateTableData();
 }
